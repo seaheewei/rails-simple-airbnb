ebd987ab55974e69ab1705f79b1c524cc258eae4
Eewei Seah <seaheewei@gmail.com>
2023-02-18 09:21:08 +0800

    index view added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Bright & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Singapore',
+  address: '10 Clifton Gardens Singapore W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Colourful & Spacious Garden Flat Taiwan',
+  address: '10 Clifton Gardens Taiwan W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 90,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Heavy & Spacious Garden Flat Azores',
+  address: '10 Clifton Gardens Azores W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 50,
+  number_of_guests: 2
+)
